53525662358db9a95cff7d343d80f1eea0ca8af2
Tom Bostelmann <tbostelmann@gmail.com>
2013-10-21 21:36:57 -0700

    added omniauth stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1424815047733630', '64f6d1e97a8c601ec9a44ec6bcc1e0ef', {:scope => 'email'}
+  provider :facebook, '1424815047733630', '64f6d1e97a8c601ec9a44ec6bcc1e0ef',
+           {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Conw::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
